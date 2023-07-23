aa385b3e98fb294576be616e5aaf4d1ca4c5c036
Diego <dap09310@gmail.com>
2023-07-23 17:49:46 +0200

    Fix install.sh

--- .setup/install.sh
+++ .setup/install.sh
@@ -16,18 +16,18 @@ cd $BASE_FOLDER
 echo "$DOTFILES_FOLDER" >> .gitignore
 
 git clone --bare $GIT_REPO_URL $DOTFILES_PATH
-alias gitdf "/usr/bin/git --git-dir=$DOTFILES_PATH --work-tree=$HOME"
+alias gitdf="/usr/bin/git --git-dir=$DOTFILES_PATH --work-tree=$HOME"
 
-config checkout
+gitdf checkout
 if [ $? = 0 ]; then
     echo "Checked out config.";
 else
     echo "Backing up pre-existing dot files in folder $(pwd)/.config-backup/";
     mkdir -p .config-backup
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    gitdf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
-config config status.showUntrackedFiles no
+gitdf checkout
+gitdf config status.showUntrackedFiles no
 
 echo "All set, remember to add ${bold}alias gitdf=\"/usr/bin/git --git-dir=$DOTFILES_PATH --work-tree=\$HOME\"${normal} to your console rc or aliases file"
 
